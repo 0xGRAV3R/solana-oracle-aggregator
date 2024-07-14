c49d727ad090f02dedb09b6f533ffeee4609316b
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-07-14 16:34:58 -0700

    ⚓ first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Oracle Aggregator for DeFi Application
+
+This project is a decentralized finance (DeFi) application designed to enhance the accuracy and reliability of SOL/USD price information. It achieves this by aggregating price feeds from multiple oracle sources, ensuring that the data is comprehensive and up-to-date. The application then calculates an average price from these diverse inputs to provide a balanced and accurate representation of the SOL/USD market value. This calculated average price is subsequently displayed on the frontend, offering users a clear and reliable view of the current market trends and valuations. Through this process, the application aims to deliver a robust and transparent price discovery mechanism for the DeFi community.
+
+## Project Overview
+
+The Oracle Aggregator retrieves price data from two distinct oracle sources:
+
+-Pyth Network
+-Dia API
+
+After gathering the data, it computes the average SOL/USD price and presents it on the frontend using React and TypeScript.
+
+## Requirements
+
+To run this project locally, ensure you have Node.js installed on your machine.
+
+## Getting Started
+
+### 1. Clone the repository:
+
+```bash
+git clone https://github.com/0xgrav3r/solana-oracle-aggregator.git
+```
+
+### 2. Navigate into the project directory:
+
+```bash
+cd solana-oracle-aggregator
+```
+
+### 3. Install dependencies using npm:
+
+```bash
+npm install
+```
+
+### 4. Run the application:
+
+```bash
+npm start
+```
+
+### 5. Open your browser and navigate to http://localhost:3000 to view the application.
---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import PythPrice from "./PythPrice";
+import DiaPrice from "./DiaPrice";
+import { NumericFormat } from "react-number-format";
+import logoSVG from "./solana-sol-logo.svg";
+
+const App: React.FC = () => {
+  const [pythPrice, setPythPrice] = useState<number | null>(null);
+  const [diaPrice, setDiaPrice] = useState<number | null>(null);
+  const [averagePrice, setAveragePrice] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (pythPrice !== null && diaPrice !== null) {
+      const average = (pythPrice + diaPrice) / 200000; // Divide by 200000 to adjust for display in millions
+      setAveragePrice(average);
+    }
+  }, [pythPrice, diaPrice]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Solana Oracle Aggregator</h2>
+      </header>
+      <main>
+        <div className="card">
+          <div className="flexbox">
+            <img
+              className="logosvg"
+              src={logoSVG}
+              alt="Solana Logo"
+            />
+            <h3>SOL/USD Average: </h3>
+            <div className="wrapper">
+              {averagePrice !== null ? (
+                <p>
+                  <NumericFormat
+                    value={averagePrice}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                    decimalScale={0}
+                  />
+                </p>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+          </div>
+        </div>
+        <div>
+          <PythPrice callback={setPythPrice} />
+          <DiaPrice callback={setDiaPrice} />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
